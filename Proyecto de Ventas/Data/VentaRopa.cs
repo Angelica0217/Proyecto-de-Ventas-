bf588c05397c92388989825050043030412d6871
Angelica0217 <157177696+Angelica0217@users.noreply.github.com>
2024-01-30 12:12:51 -0300

    cambio angelicUpdate README.md

---
+++ Proyecto de Ventas/Data/VentaRopa.cs
@@ -0,0 +1,7 @@
+﻿public class VentaRopa
+{
+    public string Foto { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public double Precio { get; set; }
+}
